0f3c92abf17d3c5ce8495cd238908057bd9ed3f7
PeerG7 <peerapon.lukc@bumail.net>
2026-05-14 20:01:58 +0700

    Bruh

--- Assets/_Scripts/Attack/Attack.cs
+++ Assets/_Scripts/Attack/Attack.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 
 public abstract class Attack : NetworkBehaviour
 {
-    public void Initialize()
+    public AttackData data { get; private set; }
+    public int level { get; private set; }
+    public void Initialize(AttackData data, int level)
     {
-
+        this.data = data;
+        this.level = level;
+        OnInitialize();
     }
     protected abstract void OnInitialize();
     public abstract void Tick();
